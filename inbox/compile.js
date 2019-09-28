c877a824d912b4f90ba5f2623310d2eb3885b7a5
TuanAnh <ncq998@gmail.com>
2019-09-28 10:16:57 +0700

    dadasdsa

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,4 +5,6 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+
+//solc.compile params 1: contract src file, 2: contract count
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
